65983c6247b67e560791e742676c61b1b52bcac7
Aalekh Nigam <aalekh.nigam@gmail.com>
2016-02-23 07:18:00 +0530

    Update queue_server.cpp

--- queue_server.cpp
+++ queue_server.cpp
@@ -1,38 +1,62 @@
-#include <iostream>
+#include <map>
+#include <stdio.h>
+#include <vector>
+#include "iostream"
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netinet/ip.h>
-#include <arpa/inet.h>
-
-//For Vpack objects
-#include <velocypack/vpack.h>
 
 using namespace std;
-using namespace arangodb::velocypack;
 
 int main()
 {
-    sockaddr_in serv;
-    bzero(&serv, sizeof(serv));
-    serv.sin_family=AF_INET;
-    serv.sin_port=htons(3345);
-    inet_aton("127.0.0.1", &(serv.sin_addr));
-
-    int servfd=socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    bind(servfd, (sockaddr*)(&serv), sizeof(serv));
-
-    listen(servfd, 5);
-    sockaddr cli;
-    int connfd;
-    pid_t id=-1;
-    socklen_t siz=sizeof(cli);
-    while(true)
+    map<uint32_t,int> messageCount;
+    int count[2];
+    //count[12345] = 5;
+    map<uint32_t, vector<string> > vpackMap;
+    char str[100], str2[4];
+    int listen_fd, comm_fd;
+ 
+    struct sockaddr_in servaddr;
+ 
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+ 
+    bzero( &servaddr, sizeof(servaddr));
+ 
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htons(INADDR_ANY);
+    servaddr.sin_port = htons(22000);
+ 
+    bind(listen_fd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+ 
+    listen(listen_fd, 10);
+ 
+    comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL);
+ 
+    while(1)
     {
-        if( (connfd=accept(servfd, &cli, &siz)) >=0 )
-            continue;
+ 
+ //       bzero( str, 100);
+ 
+        read(comm_fd, count, sizeof(count));
+        cout << "The size of recieved messageCOunt is: " << sizeof(count) << endl;
+        //cout << "Echoing back - " << str << "size" << (unsigned)strlen(str)<<  " 0: "<< str[0]<< " 1: " << str[1] << endl;
+        //if((str[0] == 'h') &&(str[1] == 'e')){
+        //    str2[0] = 't';
+        //    str2[1] = 'o'; 
+        //    str2[2] = 't';
+        //    str2[3] = 'o';;
+        //    write(comm_fd, str2, strlen(str2));
+        // }//else{
+        //     write(comm_fd, str, strlen(str)); 
+        // }
+        //else{
+        //     str = "hello-stranger, no access";
+        //     write(comm_fd, str, strlen(str)+1);
+        // }
+ 
     }
 }
